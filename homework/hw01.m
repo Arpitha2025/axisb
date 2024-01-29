fb0d0b26c836b85c09557b87d10a06074b8762a7
Arpitha2025 <108902268+Arpitha2025@users.noreply.github.com>
2024-01-29 12:39:30 -0600

    Update hw01.m

--- homework/hw01.m
+++ homework/hw01.m
@@ -1,13 +1,13 @@
 clear, close all; clc;
 
 % create a random matrix of size 7x10
-A = % ADD YOUR CODE HERE
+A = rand(7, 10)
 
 % create random vector of size 10x1
-x = % ADD YOUR CODE HEREE
+x = rand(10, 1)
 
 % compute matrix vector product
-b = % ADD YOUR CODE HERE
+b = A .* x
 
 % display result
 disp('b = Ax:');
