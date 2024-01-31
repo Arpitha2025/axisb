ae2c23a985c1ebc68c4fdb728d470faf8b06b119
Arpitha2025 <108902268+Arpitha2025@users.noreply.github.com>
2024-01-30 21:22:52 -0600

    Update hw02.m

--- homework/hw02.m
+++ homework/hw02.m
@@ -3,10 +3,10 @@ clear, close all; clc;
 % for loop from k = 2 to 5 with increment 1
 for k = 2:5
     % create random matrix of size k x 4
-    A = % ADD YOUR CODE HERE
+    A = rand(k,4);
 
     % find reduced echelon form of A
-    B = % ADD YOUR CODE HERE
+    B = rref(A);
 
     % display the result
     fprintf('reduced echelon form of %d x 4 matrix\n', k);
