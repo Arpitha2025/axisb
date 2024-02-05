c38c37c6c32cd737a46fdd3937af1a1f1cd812ea
Arpitha2025 <108902268+Arpitha2025@users.noreply.github.com>
2024-02-05 12:37:14 -0600

    Update hw03.m

--- homework/hw03.m
+++ homework/hw03.m
@@ -1,16 +1,18 @@
 clear, close all; clc;
 
 % create matrix of size 10 x 10 using the 'magic' cmd
-A = % ADD YOUR CODE HERE
+A = magic(10);
 
 % create 10 x 1 vector of all ones
-u = % ADD YOUR CODE HERE
+u = ones(10,1);
 
 % compute matrix-vector product (matvec)
-y = % ADD YOUR CODE HERE
+y = A * u
 
 % display result
 fprintf('y = \n');
 disp(y);
 
 % DESCRIBE YOUR OBSERVATIONS BELOW (as a comment)
+%a matrix of 10 rows and columns with all its elements set to 10 is multiplied by
+%a vector of 10 elements with all its elements set to 1.
