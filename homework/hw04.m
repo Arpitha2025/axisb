4d51aa705eeb01544a2153b36d5ced9c35e90bb3
Arpitha2025 <108902268+Arpitha2025@users.noreply.github.com>
2024-02-07 18:55:00 -0600

    Update hw04.m

--- homework/hw04.m
+++ homework/hw04.m
@@ -5,24 +5,24 @@ A = [31, -8, 11; -8, 15, -6; 11, -6, 25];
 b = [ 8; -4; 13];
 
 % create augmented matrix
-M = % ADD YOUR CODE HERE
+M = [A,b];
 
 % find reduced echelon form of augmented matrix
-B = % ADD YOUR CODE HERE
+B = rref(M)
 
 % get solution and print solution on screen
-x = % ADD YOUR CODE HERE
+x = B;
 
 fprintf('solution x = \n');
 disp(x);
 
 % compute error vector and display it
-errvec = % ADD YOUR CODE HERE
+errvec = lteEVM(x,r);
 
 fprintf('error vector r = Ax - b = \n');
 disp(errvec);
 
 % compute norm of error vector
-nerr = % YOUR CODE GOES HERE
+nerr = norm(errvec);
 
 fprintf('||r||_2 = %e\n', nerr);
