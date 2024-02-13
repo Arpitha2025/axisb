fd8c296c4ab7c12b55d82d8f924e66f92eead34d
Arpitha2025 <108902268+Arpitha2025@users.noreply.github.com>
2024-02-12 20:28:53 -0600

    Update hw05.m

--- homework/hw05.m
+++ homework/hw05.m
@@ -12,14 +12,24 @@ A = [A,0];
 
 % check that A*x = 0 has free variables
 fprintf('reduced echelon form of [A 0]\n');
-% ADD YOUR CODE HERE
+free = true;
+if A == 0
+     free = true;
+else
+     free = false;
 
 % inspect your result and draw conclusions
 % if there are free variables, delete one column, form a new matrix B
 % with remaining columns, and check again for free variables of new
 % augmented system [B 0]
 % ADD YOUR CODE HERE
-
+if free
+     A(:, 1) = [];
+B = A;
+if A == 0
+     free = true;
+else
+     free = false;
 
 % HINT (for advanced students):
 % This problem can be solved using a "for loop"
