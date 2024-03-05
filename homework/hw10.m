e0db98609650461249bacab1ea67012aa0538990
Arpitha2025 <108902268+Arpitha2025@users.noreply.github.com>
2024-03-04 20:21:58 -0600

    Update hw10.m

--- homework/hw10.m
+++ homework/hw10.m
@@ -6,22 +6,23 @@ b = [8; -4; 13];
 
 % create the augmented matrix
 % ADD YOUR CODE HERE
+M = [A,b]
 
 % find the reduced echelon form of augmented matrix
-B = % ADD YOUR CODE HERE
+B = rref(M)
 
 % get the solution
-x = % ADD YOUR CODE HERE
+x = B
 
 % compute the error and show it
-dr = % ADD YOUR CODE HERE
+dr = norm(Ax - b)
 fprintf('error for rref      dr = ||Ax - b||_2 = %e\n', dr);
 
 
 % compute solution of system using backslash command
-x = % ADD YOUR CODE HERE
+x = A/b;
 
 % compute the error and show it
-dr = % ADD YOUR CODE HERE
+dr = norm(Ax - b)
 fprintf('error for backslash dr = ||Ax - b||_2 = %e\n', dr);
 fprintf('\n');
