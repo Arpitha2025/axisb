c03ae29f37162fdfe774a3686865fb29aa50bfa0
Arpitha2025 <108902268+Arpitha2025@users.noreply.github.com>
2024-03-16 01:55:02 -0500

    Update hw24.m

--- homework/hw24.m
+++ homework/hw24.m
@@ -5,15 +5,15 @@ n = 2:1:7;
 for i = 1 : numel(n)
     % create a matrix of size n x n with values between 0 and 9
     % use the randi command
-    A = % ADD YOUR CODE HERE
-    B = % ADD YOUR CODE HERE
+    A = randi([0,9],n,n)
+    B = randi([0,9],n,n)
     fprintf('---------------------------------------\n');
     fprintf('for A,B %d x %d matrices\n', n(i), n(i));
     fprintf('---------------------------------------\n');
     fprintf('matrix A:\n'); disp(A);
     fprintf('matrix B:\n'); disp(B);
     % compute the difference between det(A*B) and det(A)*det(B)
-    delta = % YOUR CODE GOES HERE
+    delta = det(A*B) - det(A)*det(B);
     fprintf('det(A)*det(B) - det(A*B) = %e\n\n', delta);
 end
 
